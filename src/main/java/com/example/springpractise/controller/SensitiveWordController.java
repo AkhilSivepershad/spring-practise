a6227db465b08527944fccca1ff53a03e5b0112d
Akhil Sivepershad <akhil.sivepershad@entelect.co.za>
2023-04-19 10:29:36 +0200

    modified controller to use response handler method

--- src/main/java/com/example/springpractise/controller/SensitiveWordController.java
+++ src/main/java/com/example/springpractise/controller/SensitiveWordController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping("sensitiveWords")
@@ -14,25 +15,23 @@ public class SensitiveWordController {
     public SensitiveWordController(SensitiveWordService sensitiveWordService) {
         this.sensitiveWordService = sensitiveWordService;
     }
+    private ResponseEntity<?> handleResponse(Supplier<Object> serviceCall) {
+        try {
+            Object result = serviceCall.get();
+            return new ResponseEntity<>(result, HttpStatus.FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Word does not exist",HttpStatus.NOT_FOUND);
+        }
+    }
 
     @GetMapping()
     public ResponseEntity<?> getSensitiveWords() {
-        try {
-            return new ResponseEntity<>(sensitiveWordService.getSensitiveWords(), HttpStatus.FOUND);
-        } catch (
-                EntityNotFoundException E) {
-            return new ResponseEntity<>("No words found",HttpStatus.NOT_FOUND);
-        }
+        return handleResponse(() -> sensitiveWordService.getSensitiveWords());
     }
 
     @GetMapping("{word}")
     public ResponseEntity<?> getSensitiveWord(@PathVariable String word) {
-        try {
-            return new ResponseEntity<>(sensitiveWordService.getByWord(word), HttpStatus.FOUND);
-        } catch (
-                EntityNotFoundException E) {
-            return new ResponseEntity<>("Word does not exist",HttpStatus.NOT_FOUND);
-        }
+        return handleResponse(() -> sensitiveWordService.getByWord(word));
     }
 
     @PostMapping("create")
@@ -54,5 +53,6 @@ public class SensitiveWordController {
         catch (EntityNotFoundException E){
             return new ResponseEntity<>("Word does not exist",HttpStatus.NOT_FOUND);
         }
+//        return handleResponse(() -> sensitiveWordService.deleteSensitiveWord(word));
     }
 }
